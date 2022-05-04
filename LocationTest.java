71f187aa4b73e78f2dca132322ca502043aed0af
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2022-05-04 01:15:15 -0700

    Update on LocationTest.java

--- LocationTest.java
+++ LocationTest.java
@@ -4,22 +4,22 @@ import org.junit.jupiter.api.Assertions;
 public class LocationTest {
     
     @Test
-    //Testing object instance and lowerCase
+    //Tests object instance and lowerCase
     public void cityNameToLowerCase(){
         Location location = new Location("Fresno");
         Assertions.assertEquals("fresno", location.getCityName());
         Assertions.assertNotEquals("Fresno", location.getCityName());
-
     }
 
     @Test
-    //testing setNewCityName function
+    //tests setNewCityName function
     public void newCityName(){
         Location location = new Location("Fresno");
         Assertions.assertEquals("santa barbara", location.setNewCityName("Santa Barbara"));
     }
 
     @Test
+    //Tests main function. Make sure when city exists, it does not return null values
     public void mainDoesNotReturnNull(){  
         String city = "Fresno";
         Location location = new Location("Fresno");
@@ -28,10 +28,24 @@ public class LocationTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     @Test
+    //Testing main functions for city that do not exist or cities with typos
+    //This is supposed to return null values
+    //This will be used to validate user input
+    public void mainReturnsNull(){
+        String city = "ahahahaha";
+        Location location = new Location(city);
+        try {
+            Assertions.assertNull(location.main(city));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    //Tests if getCityWeatherData does not return null for valid city 
     public void getCityWeatherDataDoesNotReturnNull(){
         Location location = new Location("Fresno");
         try {
@@ -42,6 +56,7 @@ public class LocationTest {
     }
     
     @Test
+    //tests if we are able to extract the basic info
     public void extractingBasicInfoDoesNotReturnNull(){
         String fileName = "data.json";
         Location location = new Location("Fresno");
@@ -53,6 +68,7 @@ public class LocationTest {
     }
 
     @Test
+    //Tests the function that will map nested objects returned by API
     public void jsonToMapDoesNotReturnNull(){
         String city = "Fresno";
         Location location = new Location("Fresno");
@@ -65,6 +81,7 @@ public class LocationTest {
     }
 
     @Test
+    //Tests the function that sends the request to the API and returns a string
     public void getDataFromAPIDoesNotReturnNull(){
         String city = "Fresno";
         Location location = new Location("Fresno");
